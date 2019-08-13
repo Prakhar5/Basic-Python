c721f8284e08d8886d836d035ad9e71f42f5b70c
Prakhar5 <52296296+Prakhar5@users.noreply.github.com>
2019-08-14 02:23:11 +0530

    Add files via upload

---
+++ TCS que complete solution.py
@@ -0,0 +1,31 @@
+from array import *
+prime=[]
+x,y=input("ENTER").split()
+
+x=int(x)
+y=int(y)
+
+for val in range(x, y + 1):
+
+
+   if val > 1:
+       for n in range(2, val):
+           if (val % n) == 0:
+               break
+       else:
+
+           prime.append(val)
+
+result=[]
+k=len(prime)
+for i in range(k):
+    for j in range(i):
+        x=prime[i]-prime[j]
+        if x==6:
+            result.append(prime[i])
+            result.append(prime[j])
+            print("Pair found (",prime[i],",",prime[j],")",end="")
+l=len(prime)
+print()
+print(l/2)
+
